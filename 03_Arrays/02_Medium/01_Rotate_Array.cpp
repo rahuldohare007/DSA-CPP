74c2b258891d26f2f6ff0f90501ad19df4b73a37
rahuldohare007 <rahuldohare401@gmail.com>
2026-09-09 13:09:34 +0530

    Arrays

--- 03_Arrays/02_Medium/01_Rotate_Array.cpp
+++ 03_Arrays/02_Medium/01_Rotate_Array.cpp
@@ -1,31 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void rotateArray(vector<int>& arr, int k) {
-    int n = arr.size();
-    k = k % n; // Handle cases where k is greater than n
-    reverse(arr.begin(), arr.end());
-    reverse(arr.begin(), arr.begin() + k);
-    reverse(arr.begin() + k, arr.end());
+void rotateArray(vector<int>& arr, int k, string dir)
+{
+    int size = arr.size();
+
+    if (size == 0 || k == 0) return;
+
+    k = k % size;
+
+    if (dir == "right")
+    {
+        reverse(arr.begin(), arr.end());
+        reverse(arr.begin(), arr.begin() + k);
+        reverse(arr.begin() + k, arr.end());
+    }
+    else if (dir == "left")
+    {
+        reverse(arr.begin(), arr.begin() + k);
+        reverse(arr.begin() + k, arr.end());
+        reverse(arr.begin(), arr.end());
+    }
 }
 
-int main() {
+int main()
+{
     vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
     int k = 3;
+    string dir = "right";
 
-    rotateArray(arr, k);
+    rotateArray(arr, k, dir);
 
-    for (int num : arr) {
+    for (int num : arr)
         cout << num << " ";
-    }
-    cout << endl;
- 
+
     return 0;
 }
 
-// Example 
+// Example - Right
 // Input: arr = [1, 2, 3, 4, 5, 6, 7], k = 3
 // Output: 5 6 7 1 2 3 4
+
+// Example - Left
+// Input: arr = [1, 2, 3, 4, 5, 6, 7], k = 3
+// Output: 4 5 6 7 1 2 3
 
 // Time Complexity: O(n), where n is the number of elements in the array.
 // The reverse operation takes linear time, and we perform it three times.
